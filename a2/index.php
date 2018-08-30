c5ad1dd5b65c73d962da505069b633c6e1811c3d
s3707035 <s3707035@student.rmit.edu.au>
2018-08-30 12:52:09 +1000

    all done

--- a2/index.php
+++ a2/index.php
@@ -27,7 +27,7 @@
 		<a href="#Aboutus" style="text-decoration:none">
 			   <p class="aboutus">ABOUT</p></a>
 		
-		<a href="Contactus" style="text-decoration:none">
+		<a href="#Contactus" style="text-decoration:none">
 			   <p class="contactus">CONTACT</p></a>
 		
 		
@@ -52,20 +52,33 @@
 			</tr>
 			<tr>
 			<td>
-				<p class="history">About Us</p>
-			<p class="historyaboutus">sjdfkajdshfkladshflksdah
-					sadfjhaskdfjhd
-					dsfjzdslkjfhzkdshfzsdhfkjsdhzlkfj
-					ksdjzfhlzkdshflkjszdhflkzjsdfh
-					sDJFhdskjfhklsdjhfkl
-				<a id="Aboutus">DJZfhdskfhkjdszf</a>
+				<a id="Aboutus"><p class="history">About Us</p></a>
+			<p class="historyaboutus">Sell My Stuff was started by Sally Klein. Sally, a mother of seven, often discovered that other mothers in her neighborhood were purchasing new items for their children when the same items were being discarded by other families as their own children no longer needed them. She organized a monthly meet-up group where members of the community could meet, trade and donate children’s things. Sally realized that a shop was more efficient and soon opened “Children’s Things” and bought no longer needed items from mother’s and resold them to others. As business continued, she soon expanded to taking anything and everything under the sun.<br><br>
+Currently, her daughter Janice leads Sell My Stuff.com and is embarking on transforming this small community based shop into a well known Melbourne online not-for-profit based organization with the hopes of expanding into the rest of Victoria and the neighbouring states.
 					</p>
 				
 </td>
 				
 			
-</tr>
-
+			</tr>
+<tr>
+			<td>
+				<p class="phone">Contact Us</p>
+			<p class="phoneus">Unit 0000<br>
+				28371 Main Street,
+				Melbourne,
+				Victoria, <br>
+				Australia, 3000.<br><br>
+				
+			Office Hours:
+				<br>Monday-Friday 9am-5pm
+				<br>
+				03 9000-0000<br><br>
+	<a id="Contactus">Email:<br>
+		info@sellmystuff.com.au</a>
+				</p>
+				</td>
+			</tr>
   
        </table>
 		
